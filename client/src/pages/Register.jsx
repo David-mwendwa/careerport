13091b73c4442a712cb22ebeb617b38f0f4078c6
techdave <klatzdavie19@gmail.com>
2024-01-14 16:37:48 +0300

    build nested routes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-  return <div>Register page</div>;
+  return (
+    <div>
+      <h1>Register page</h1>
+      <Link to='/login'>Login Page</Link>
+    </div>
+  );
 };
 
 export default Register;
