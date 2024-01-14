a8ce37bc5918786760f3f92e84b63737b45c629c
techdave <klatzdavie19@gmail.com>
2024-01-14 16:18:40 +0300

    setup index.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import HomeLayout from './pages/HomeLayout';
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomeLayout /> },
-  { path: '/about', element: <h1>About page</h1> },
+  { path: '/register', element: <Register /> },
+  { path: '/login', element: <Login /> },
+  { path: '/dashboard', element: <DashboardLayout /> },
+  { path: '/error', element: <Error /> },
 ]);
 
 function App() {
