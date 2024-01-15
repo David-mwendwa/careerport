bb0ab7001f6223a6db0e5679328c4058c4f1ac9d
techdave <klatzdavie19@gmail.com>
2024-01-15 18:05:02 +0300

    build a landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,35 @@
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+
 const Landing = () => {
-  return <div>Landing page</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <h3 className='logo'>CAREERPORT</h3>
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            saepe temporibus nulla suscipit. Nostrum, pariatur dignissimos
+            soluta accusamus voluptate tempora sit illum numquam voluptatibus
+            totam fugiat porro maiores placeat laboriosam.
+          </p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn register-link'>
+            Login
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
