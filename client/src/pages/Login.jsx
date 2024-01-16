c830e5902bb475107e413f1699338ae31531153b
techdave <klatzdavie19@gmail.com>
2024-01-16 06:43:25 +0300

    create login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,41 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login page</h1>
-      <Link to='/register'>Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
+
+        <FormRow
+          labelText='email'
+          type='email'
+          name='email'
+          defaultValue='david@default.com'
+        />
+        <FormRow
+          labelText='password'
+          type='password'
+          name='password'
+          defaultValue='david1234'
+        />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+        <p>
+          Not a member?{' '}
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
