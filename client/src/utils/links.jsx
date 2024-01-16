969335e4c24af6da14f34b7d8297e5284caee670
techdave <klatzdavie19@gmail.com>
2024-01-16 15:20:04 +0300

    structure small sidebar

--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -1,15 +1,15 @@
-import { IosBarChartSharp } from 'react-icons/io5';
+import { IoBarChartSharp } from 'react-icons/io5';
 import { MdQueryStats } from 'react-icons/md';
 import { FaWpforms } from 'react-icons/fa';
 import { ImProfile } from 'react-icons/im';
-import { MdAdminPanelSetting } from 'react-icons/md';
+import { MdAdminPanelSettings } from 'react-icons/md';
 
 const links = [
   { text: 'add job', path: '.', icon: <FaWpforms /> },
   { text: 'all jobs', path: 'all-jobs', icon: <MdQueryStats /> },
-  { text: 'stats', path: 'stats', icon: <IosBarChartSharp /> },
+  { text: 'stats', path: 'stats', icon: <IoBarChartSharp /> },
   { text: 'profile', path: 'profile', icon: <ImProfile /> },
-  { text: 'admin', path: 'admin', icon: <MdAdminPanelSetting /> },
+  { text: 'admin', path: 'admin', icon: <MdAdminPanelSettings /> },
 ];
 
 export default links;
