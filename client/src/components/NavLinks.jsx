923d63c990dbb2c9ac42814a2c33ce9d479db2bc
techdave <klatzdavie19@gmail.com>
2024-01-16 16:13:44 +0300

    isolate navlinks in its own component

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,27 @@
+import { useDashboardContext } from '../pages/DashboardLayout';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = () => {
+  const { toggleSidebar } = useDashboardContext();
+  return (
+    <div className='nav-links'>
+      {links.map((link) => {
+        const { text, path, icon } = link;
+        return (
+          <NavLink
+            className='nav-link'
+            to={path}
+            key={text}
+            onClick={toggleSidebar}
+            end>
+            <span className='icon'>{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
