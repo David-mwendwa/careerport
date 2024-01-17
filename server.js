7ed5dbc33358246a919e54d9314d494ae82ac13c
techdave <klatzdavie19@gmail.com>
2024-01-17 09:55:34 +0300

    setup server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+import morgan from 'morgan';
+dotenv.config();
 import express from 'express';
+import dotenv from 'dotenv';
+
 const app = express();
+
+app.use(express.json());
+if (!/production/.test(process.env.NODE_ENV)) {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(5100, () => {
-  console.log('server running....');
+app.post('/', (req, res) => {
+  res.json({ message: 'Data received via POST requirest', data: req.body });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}...`);
 });
