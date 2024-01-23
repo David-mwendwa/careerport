29cc5229a4a6366e770241b845636349dc3b5535
techdave <klatzdavie19@gmail.com>
2024-01-23 07:41:28 +0300

    create user validation logic

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -3,6 +3,7 @@ import { body, param, validationResult } from 'express-validator';
 import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 import Job from '../models/Job.js';
+import User from '../models/User.js';
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -40,4 +41,24 @@ export const validateIdParam = withValidationErrors([
     const job = await Job.findById(value);
     if (!job) throw new NotFoundError(`No job with id: ${value}`);
   }),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body('name').notEmpty().withMessage('name is required'),
+  body('email')
+    .notEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('invalid email format')
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) throw new BadRequestError('email already exists');
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 6 })
+    .withMessage('password must be at least 6 characters long'),
+  body('location').notEmpty().withMessage('location is required'),
+  body('lastName').notEmpty().withMessage('lastName is required'),
 ]);
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { login, register } from '../controllers/user.js';
+import { validateRegisterInput } from '../middleware/validationMiddleware.js';
 
 const router = Router();
 
-router.route('/register').post(register);
+router.route('/register').post(validateRegisterInput, register);
 router.route('/login').post(login);
 
 export default router;
