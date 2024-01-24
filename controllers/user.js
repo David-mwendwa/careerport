71438cd3b2f336e99329b0328b6d97aa2c5782c4
techdave <klatzdavie19@gmail.com>
2024-01-24 15:07:33 +0300

    setup http only cookie Cookie that can't be accessed by js running in the browser. It's designed to help prevent XSS attacks which can be used to steal cookies and other sensitive information

--- controllers/user.js
+++ controllers/user.js
@@ -23,6 +23,13 @@ export const login = async (req, res) => {
   const isValidUser =
     user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
+  
   const token = createJWT({ userId: user._id, role: user.role });
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
   res.status(StatusCodes.OK).json({ success: true, token });
 };
