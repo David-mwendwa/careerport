c1c693638ce085fc7987a8a85412efdad75b43de
techdave <klatzdavie19@gmail.com>
2024-01-29 09:47:10 +0300

    set up react-toastfy for alerts

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,15 +3,17 @@ import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   try {
     await customFetch.post('/auth/register', data);
+    toast.success('registration successfull');
     return redirect('/login');
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.message);
     return error;
   }
 };
