ce6240757cdca947882e49db9eed85815a2d107d
techdave <klatzdavie19@gmail.com>
2024-02-01 10:07:32 +0300

    build jobs container

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,5 +1,27 @@
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import Job from './Job';
+
 const JobsContainer = () => {
-  return <h1>JobsContainer</h1>;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+
+  if (!jobs.length) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
