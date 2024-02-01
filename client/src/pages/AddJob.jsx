0aabf32fd14a230e9718ddfd580ad52faf169602
techdave <klatzdavie19@gmail.com>
2024-02-01 08:13:02 +0300

    structure add job page

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,35 @@
-const AddJob = () => {
-	return (
-		<div>AddJob</div>
-	)
-}
+import { Form, useNavigation, useOutletContext } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { FormRow } from '../components';
 
-export default AddJob
+const AddJob = () => {
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = /submitting/.test(navigation.state);
+
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>add job</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='position' />
+          <FormRow type='text' name='company' />
+          <FormRow
+            type='text'
+            name='jobLocation'
+            labelText='job location'
+            defaultValue={user.location}
+          />
+          <button
+            type='submit'
+            className='btn btn-block form-btn'
+            disabled={isSubmitting}>
+            {isSubmitting ? 'submitting' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default AddJob;
