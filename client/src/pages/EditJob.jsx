e7f12b34e69c7085f606503487bc948e384a5664
techdave <klatzdavie19@gmail.com>
2024-02-05 09:06:02 +0300

    create edit-job jsx

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,5 +1,8 @@
-import { redirect, useLoaderData } from 'react-router-dom';
+import { Form, redirect, useLoaderData, useNavigation } from 'react-router-dom';
 import customFetch from '../utils/customFetch';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { FormRow, FormRowSelect } from '../components';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 
 export const loader = async ({ params }) => {
   try {
@@ -17,9 +20,44 @@ export const action = async () => {
 
 const EditJob = () => {
   const { job } = useLoaderData();
-  console.log(job)
+  const navigation = useNavigation();
+  const isSubmitting = /submitting/.test(navigation.state);
 
-  return <div>EditJob page</div>;
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>edit job</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='position' defaultValue={job.position} />
+          <FormRow type='text' name='company' defaultValue={job.company} />
+          <FormRow
+            type='text'
+            name='jobLocation'
+            labelText='job location'
+            defaultValue={job.jobLocation}
+          />
+          <FormRowSelect
+            name='jobStatus'
+            labelText='job status'
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type='submit'
+            className='btn btn-block form-btn'
+            disabled={isSubmitting}>
+            {isSubmitting ? 'submitting...' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default EditJob;
