8c48cf267bc30e0418d03350a1934687b11c9b57
techdave <klatzdavie19@gmail.com>
2024-02-07 08:45:26 +0300

    create profile action

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,24 @@
 import { Form, useNavigation, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { FormRow } from '../components/index';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const file = formData.get('avatar');
+  if (file && file.size > 5000000) {
+    toast.error('Image size too large');
+    return null;
+  }
+  try {
+    await customFetch.patch('/users/update-user', formData);
+    toast.success('Profile updated successfully');
+  } catch (error) {
+    toast.error(error?.response?.data?.message || error?.message);
+  }
+  return null;
+};
 
 const Profile = () => {
   const { user } = useOutletContext();
