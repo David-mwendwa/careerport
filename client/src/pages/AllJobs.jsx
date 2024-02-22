fe51c2aaad46f45dcbf457c30c5b640c306f843e
techdave <klatzdavie19@gmail.com>
2024-02-22 22:30:32 +0300

    rm

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -4,9 +4,11 @@ import { JobsContainer, SearchContainer } from '../components';
 import { createContext, useContext } from 'react';
 
 export const loader = async ({ request }) => {
+  console.log(request.url);
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
   ]);
+  console.log(params);
   try {
     const { data } = await customFetch.get('/jobs', { params });
     return { data };
